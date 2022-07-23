39e73a04ccb8c9c32ea8bf50ac5eeb05b3df40f8
Marcella <109836529+Marecella@users.noreply.github.com>
2022-07-23 11:57:49 -0300

    Alteração3

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache
+
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -15,7 +17,7 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-    # Create a text element and let the reader know the data is loading.
+# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
@@ -23,3 +25,4 @@ data = load_data(10000)
 data_load_state.text('Loading data...done!')
 
 
+data_load_state.text("Done! (using st.cache)")
